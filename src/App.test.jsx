5e995144960c14709227833bcc4050d74fdf6c3d
singhaditya3116 <singhaditya3116@gmail.com>
2026-05-06 11:07:33 +0530

    setup ready

---
+++ src/App.test.jsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react'
+import { test, expect } from 'vitest'
+import App from './App'
+
+test('renders app', () => {
+  render(<App />)
+  expect(screen.getByText(/get started/i)).toBeInTheDocument()
+})
+
+test('renders app 2', () => {
+  render(<App />)
+  expect(screen.getByText(/get started/i)).toBeInTheDocument()
+}) 
